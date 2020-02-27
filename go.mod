89235f89b45b669c85dc3321041369fcf15cb23d
Thang Vo <thang.vo@taplifetechnology.com>
2020-02-27 17:08:32 +0700

    + ADD Postgres connection

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.3.0
 	github.com/simagix/gox v0.0.0-20200126005658-91ad21a3961a // indirect
 	github.com/simagix/keyhole v1.5.4 // indirect
 	github.com/simagix/mongo-ftdc v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	client "github.com/vtdthang/goapi/drivers/mongo"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/vtdthang/goapi/routers"
 )
 
 func main() {
-
-	client.ConnectSingleton()
+	connectToPostgres()
 
 	router := routers.InitRoutes()
 	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+func connectToPostgres() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+
+	psqlInfo := os.Getenv("PG_LOCAL_URL")
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
 }
